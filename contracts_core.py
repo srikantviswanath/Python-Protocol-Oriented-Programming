a9d9b6e69dc37f640efb331fff0cc6c7baadf8e0
srikantviswanath <srikantviswanath8@gmail.com>
2017-05-15 22:17:09 -0400

    Added decorator functions @required and @borrow that define the mandatory methods of Contract class and default sharable implementations across Conformee class, respectively

--- contracts_core.py
+++ contracts_core.py
@@ -29,7 +29,7 @@ def Contract(*Conformers):
 					for k, v in ConformerClass.__dict__.iteritems():
 						if callable(v):
 							if k not in Conformee.__dict__:
-								if v.func_name == 'required':
+								if v.func_name == 'requiredWrapper':
 									self.missingReqdMethods[ConformerClass.__name__].extend(['{method}()'.format(method=k)])
 								if v.func_name == 'borrowWrapper' and k not in Conformee.__dict__:
 									setattr(self.__class__, k, v)
@@ -46,3 +46,61 @@ def Contract(*Conformers):
 
 		return ContractWrapper
 	return ContractDecorator
+
+
+def required(method):
+	'''
+	decorator to deem a method of Contract class as a mandatory method, intended to be implemented
+	by the conformee class abiding by the Contract
+
+	:param function method:
+	'''
+
+	def requiredWrapper(self, *args):
+		raise ContractRequiredMethodInvoked(method, self)
+	return required
+
+extensionRegistry = dict()
+def borrow(*dpeendencies):
+	'''
+	decorator to deem an instance method of Contract class to be borrowed by abiding Conformee classes.
+	i.e., This is the place to implement common functionality for all conforming classes ala Swift protocol extensions
+
+	:param tuple(<type>) dependencies: These are used to profide default implementations based on type-dependency 
+										inference for multidispatch of methods
+	'''
+	def borrowDecorator(method):
+		methodMetaKey = method.__module__ + ':' + method.__name__
+		regKey = tuple((tuple(map(lambda x: x.__name__, dependencies or tuple((object, )))), methodMetaKey))
+		if regKey in extensionRegistry:
+			pass #TODO: Log duplicate registration
+		extensionRegistry[regKey] = method
+		def borrowWrapper(self, *args):
+			objDeps = map(lambda x: x.__name__, self.__protocols__)
+			objDeps.extend([self.__class__.__base__.__base__.__name__])
+			matchingDep = _bestFit(objDeps)
+			return extensionRegistry[tuple((matchingDep, methodMetaKey))][self, *args]
+		return borrowWrapper
+	return borrowDecorator
+
+def _bestFit(conformeeDeps):
+	'''
+	helper function determine the best type bundle fit amongst a bunch of type registrations registered
+	at global dictionary extensionRegistry
+
+	:param tuple(<type>) conformeeDeps: The actual dependecies of the Conformee class, i.e, parent classes and 
+										protocol classes
+	'''
+	default = tuple((object.__name__, ))
+	match = default
+	for k in extensionRegistry:
+		k = k[0]
+		if set(k).issubset(set(conformeeDeps)):
+			if len(k) == len(match):
+				if match != default:
+					match = default
+				else: 
+					match = k
+			elif len(k) > len(match):
+				match = k
+	return match
